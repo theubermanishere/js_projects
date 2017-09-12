866188517c209e7720aefdb5beef2a59af6d6108
Vinayak <vinayakkaushikk@gmail.com>
2017-09-12 10:29:50 +0530

    Censored Keyboard Complete

--- custom/censoredKeyboard.js
+++ custom/censoredKeyboard.js
@@ -1,14 +1,13 @@
 var key = document.querySelector('#censored');
 
-const censoredLetters = ['q','w','x']
+const censoredLetters = {1:'q',2:'w',3:'x'}
 
 function censor(e) {
     for (var letter in censoredLetters)
     {
-        if (key.value.toLowerCase().endsWith(letter))
+        if (key.value.toLowerCase().endsWith(censoredLetters[letter]))
             {
             key.value = key.value.slice(0,-1);
-                console.log(key.value);
             }
         }
 }
